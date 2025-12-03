04c42a0bf765d79bee25eb133f99a047dca39f51
Niklas <Niklas_Schmitt11@web.de>
2025-12-03 13:55:24 +0100

    added basic login-dashboard routing and login-functionality (hardcoded credentials for now)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './scenes/LoginPage';
 import Dashboard from './scenes/Dashboard';
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <div className='app'>
-      {isAdmin 
-              ? <Dashboard />
-              : <LoginPage onLogin={setIsAdmin} />
-      } 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage onLogin={setIsAdmin} />} />
+        <Route path='/dashboard' element={isAdmin ? <Dashboard /> : <Navigate to='/login' />} />
+        <Route path='/' element={<Navigate to={isLoggedIn ? '/dashboard' : '/login'} />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/scenes/LoginPage.tsx
+++ frontend/src/scenes/LoginPage.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/LoginPage.css';
+import { useNavigate } from 'react-router-dom';
 
 interface LoginPageProps {
     onLogin: (isAdmin: boolean) => void
 }
 
 const LoginPage = ({ onLogin }: LoginPageProps) => {
-  return (
+    const [user, setUser] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string>('');
+
+    const navigate = useNavigate();
+
+    const handleLogin = (): void => {
+        if (user === 'admin' && password === 'password') {
+            onLogin(true);
+            navigate('/dashboard');
+        } else {
+            setError('Invalid Username or Password');
+        }
+    }
+
+    return (
     <div className='login-page-container'>
         <div className='login-page'>
             <h1>Login</h1>
-            <input placeholder='Username' />
-            <input placeholder='Password' />
-            <button onClick={() => onLogin(true)}>Login</button>
+            <input placeholder='Username' onChange={(e) => {setUser(e.target.value); setError('')}} />
+            <input placeholder='Password' onChange={(e) => {setPassword(e.target.value); setError('')}} type='password'/>
+            <button onClick={() => handleLogin()}>Login</button>
+            <span style={{color: 'red'}}>{error}</span>
         </div>
     </div>
   )
