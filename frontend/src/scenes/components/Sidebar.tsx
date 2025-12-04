37f143bf653cdfbf06ec861d539684259448909f
Niklas <Niklas_Schmitt11@web.de>
2025-12-04 11:02:11 +0100

    added the sidebar component, added a logged-in state

---
+++ frontend/src/scenes/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/components/Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>Sidebar</div>
+  )
+};
+
+export default Sidebar;
