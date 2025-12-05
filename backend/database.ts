2b44f3e790f3ae18dc7ad3eaa3caf208bd01bd80
Niklas <Niklas_Schmitt11@web.de>
2025-12-05 13:14:19 +0100

    better sqlite setup added endpoint for login

---
+++ backend/database.ts
@@ -0,0 +1,60 @@
+import Database from 'better-sqlite3';
+
+const db = new Database('roomy.db');
+
+//Create table for rooms
+db.exec(`
+    CREATE TABLE IF NOT EXISTS rooms 
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        capacity INTEGER NOT NULL,
+        location TEXT
+    )    
+`);
+
+//Create table for users
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users 
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT UNIQUE,
+        is_admin BOOLEAN DEFAULT 0,
+        username TEXT UNIQUE,
+        password TEXT
+    )    
+`);
+
+export interface User {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email?: string;
+    is_admin: boolean;
+    username: string;
+    password: string;
+}
+
+interface UserCount {
+    count: number;
+}
+
+//Add default admin
+const userCount = db.prepare(`
+    SELECT COUNT(*) AS count  
+    FROM users;
+`).get() as UserCount;
+
+if (Number(userCount.count) === 0) {
+    db.prepare(`
+        INSERT INTO users (first_name, last_name, is_admin, username, password)
+        VALUES ('default', 'admin', 1, 'admin', 'password');
+    `).run();
+}
+
+
+//Create table for bookings
+
+export default db;
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import db, {User} from './database';
+import cors from 'cors';
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.use(express.json());
+app.use(cors({methods: ['GET', 'POST', 'PUT', 'DELETE']}));
+
+app.post('/login', (req, res) => {
+    const sql: string = `
+        SELECT *
+        FROM users
+        WHERE username = ? AND password = ?;
+    `;
+
+    const username: string = req.query.username as string;
+    const password: string = req.query.password as string;
+
+    const user: User | unknown = db.prepare(sql).get([username, password]);
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({message: 'User not found'});
+    }
 });
 
 app.listen(PORT, () => {
