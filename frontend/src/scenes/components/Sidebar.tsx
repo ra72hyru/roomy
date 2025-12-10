765ea2d20b967ea6a282b5a4ed4819b00bbf804f
Niklas <Niklas_Schmitt11@web.de>
2025-12-10 12:04:00 +0100

    added some styling to the sidebar

--- frontend/src/scenes/components/Sidebar.tsx
+++ frontend/src/scenes/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../styles/components/Sidebar.css';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../Authorization';
 
 const Sidebar = () => {
@@ -8,14 +8,22 @@ const Sidebar = () => {
     const {user, logout} = useAuthContext();
 
     return (
-        <div className='sidebar'>
-            Sidebar<br></br>
-            <button onClick={() => navigate('/rooms')}>Rooms</button><br></br>
-            {Boolean(user?.is_admin) && <><button onClick={() => navigate('/users')}>Users</button><br></br></>}
-            <button onClick={() => navigate('/bookings')}>Bookings</button><br></br>
+        <div className='sidebar-container'>
+            <nav className='sidebar-nav'>
+                <NavLink to='/rooms' className={({isActive}) => isActive ? 'active-page page-link' : 'page-link'}>All Rooms</NavLink>
+                {Boolean(user?.is_admin) && <NavLink to='/users' className={({isActive}) => isActive ? 'active-page page-link' : 'page-link'}>Users</NavLink>}
+                <NavLink to='/bookings' className={({isActive}) => isActive ? 'active-page page-link' : 'page-link'}>Bookings</NavLink>
+            </nav>
             <button onClick={() => logout()}>Logout</button>
         </div>
     )
+    {/* <div className='sidebar'>
+        Sidebar<br></br>
+        <button onClick={() => navigate('/rooms')}>Rooms</button><br></br>
+        {Boolean(user?.is_admin) && <><button onClick={() => navigate('/users')}>Users</button><br></br></>}
+        <button onClick={() => navigate('/bookings')}>Bookings</button><br></br>
+        <button onClick={() => logout()}>Logout</button>
+    </div> */}
 };
 
 export default Sidebar;
